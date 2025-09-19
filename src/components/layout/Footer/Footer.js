993b6f12f01c3af6a8edca4516f055e98845d344
Tatitatouu <tatiana68270wi@gmail.com>
2025-09-19 16:06:37 +0200

    footer Linkedin

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -12,6 +12,7 @@ const Footer = () => {
           <div className="qizuna-footer-simple-logo">
             <img src="/logo.webp" alt="Qizuna" className="qizuna-logo-img" />
           </div>
+          
           <div className="qizuna-footer-simple-links">
             <button 
               onClick={() => setIsLegalModalOpen(true)}
@@ -25,6 +26,33 @@ const Footer = () => {
             <span className="qizuna-footer-copyright">
               &copy; 2025 Qizuna SAS
             </span>
+            
+            {/* LinkedIn Links Section */}
+            <div className="qizuna-linkedin-title">Suivez-nous</div>
+              <div className="qizuna-linkedin-links">
+                <a 
+                  href="https://www.linkedin.com/in/mathieu-capon-56a5561a0/" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="qizuna-linkedin-link"
+                >
+                  <svg className="qizuna-linkedin-icon" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                  </svg>
+                  <span>Mathieu Capon</span>
+                </a>
+                <a 
+                  href="https://www.linkedin.com/company/qizuna/" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="qizuna-linkedin-link qizuna-linkedin-company"
+                >
+                  <svg className="qizuna-linkedin-icon" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                  </svg>
+                  <span>QIZUNA</span>
+                </a>
+              </div>
           </div>
         </div>
       </footer>
